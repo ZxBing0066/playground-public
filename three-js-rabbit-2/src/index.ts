e0f31190e7e11fd3e50d7dbaa9fa4b5f8f78be43
ZxBing0066 <ZxBing0066@gmail.com>
2023-02-08 11:43:08 +0800

    update

--- three-js-rabbit-2/src/index.ts
+++ three-js-rabbit-2/src/index.ts
@@ -2,7 +2,6 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 
 const loader = new GLTFLoader();
@@ -16,50 +15,34 @@ let app = {
     controls: null
 };
 
-let idleAction, walkAction, mixer, model, stats;
+let idleAction, walkAction, deadAction, mixer, model;
 
 const controller = {
     idle: () => {
-        setWeight(idleAction, 1);
-        setWeight(walkAction, 0);
-        actions.idle.weight = 1;
-        actions.walk.weight = 0;
+        idleAction.play();
+        walkAction.stop();
+        deadAction.stop();
     },
     walk: () => {
-        setWeight(idleAction, 0);
-        setWeight(walkAction, 1);
-        actions.idle.weight = 0;
-        actions.walk.weight = 1;
+        idleAction.stop();
+        walkAction.play();
+        deadAction.stop();
     },
-    idleToWalk: () => {
-        prepareCrossFade(idleAction, walkAction, 0.5);
+    dead: () => {
+        idleAction.stop();
+        walkAction.stop();
+        deadAction.play();
     },
-    walkToIdle: () => {
-        prepareCrossFade(walkAction, idleAction, 0.5);
-    },
-    scale: 0.4,
+    scale: 2,
     rotateY: Math.PI / 2,
     switchDuration: 0.4,
     timeScale: 1
 };
 
-let idleController, walkController, idleToWalkController, walkToIdleController;
-
-let currentBaseAction = 'idle';
-const crossFadeControls = [];
-const actions = {
-    idle: { weight: 1, action: null },
-    walk: { weight: 0, action: null }
-};
-
-function modifyTimeScale(speed) {
-    mixer.timeScale = speed;
-}
-
 const createPanel = () => {
     const panel = new GUI({ width: 310 });
     panel.add(document, 'title');
-    panel.add(controller, 'scale', 0.1, 2, 0.01).onChange(value => {
+    panel.add(controller, 'scale', 0.1, 10, 0.01).onChange(value => {
         model.scale.set(value, value, value);
     });
     panel.add(controller, 'rotateY', -Math.PI, Math.PI, 0.1).onChange(value => {
@@ -75,99 +58,10 @@ const createPanel = () => {
         app.scene.background = new THREE.Color(value);
     });
 
-    const folder = panel.addFolder('Animate');
-    idleController = folder.add(controller, 'idle');
-    walkController = folder.add(controller, 'walk');
-    // idleToWalkController = folder.add(controller, 'idleToWalk');
-    // walkToIdleController = folder.add(controller, 'walkToIdle');
-    folder
-        .add(actions.idle, 'weight', 0, 1)
-        .name('idleWeight')
-        .listen()
-        .onChange(value => {
-            setWeight(idleAction, value);
-        });
-    folder
-        .add(actions.walk, 'weight', 0, 1)
-        .name('walkWeight')
-        .listen()
-        .onChange(value => {
-            setWeight(walkAction, value);
-        });
-    // folder.add(controller, 'switchDuration', 0.1, 2, 0.1);
-    folder.add(controller, 'timeScale', 0.0, 2, 0.01).onChange(modifyTimeScale);
+    panel.add(controller, 'idle');
+    panel.add(controller, 'walk');
+    panel.add(controller, 'dead');
 };
-
-function setWeight(action, weight) {
-    action.setEffectiveTimeScale(1);
-    action.setEffectiveWeight(weight);
-}
-
-function prepareCrossFade(startAction, endAction, duration) {
-    // If the current action is 'idle', execute the crossfade immediately;
-    // else wait until the current action has finished its current loop
-
-    if (currentBaseAction === 'idle' || !startAction || !endAction) {
-        executeCrossFade(startAction, endAction, duration);
-    } else {
-        synchronizeCrossFade(startAction, endAction, duration);
-    }
-
-    // Update control colors
-
-    if (endAction) {
-        const clip = endAction.getClip();
-        currentBaseAction = clip.name;
-    } else {
-        currentBaseAction = 'None';
-    }
-
-    crossFadeControls.forEach(function (control) {
-        const name = control.property;
-
-        if (name === currentBaseAction) {
-            control.setActive();
-        } else {
-            control.setInactive();
-        }
-    });
-}
-
-function synchronizeCrossFade(startAction, endAction, duration) {
-    mixer.addEventListener('loop', onLoopFinished);
-
-    function onLoopFinished(event) {
-        if (event.action === startAction) {
-            mixer.removeEventListener('loop', onLoopFinished);
-
-            executeCrossFade(startAction, endAction, duration);
-        }
-    }
-}
-
-function executeCrossFade(startAction, endAction, duration) {
-    // Not only the start action, but also the end action must get a weight of 1 before fading
-    // (concerning the start action this is already guaranteed in this place)
-
-    if (endAction) {
-        setWeight(endAction, 1);
-        endAction.time = 0;
-
-        if (startAction) {
-            // Crossfade with warping
-
-            startAction.crossFadeTo(endAction, duration, true);
-        } else {
-            // Fade in
-
-            endAction.fadeIn(duration);
-        }
-    } else {
-        // Fade out
-
-        startAction.fadeOut(duration);
-    }
-}
 
 const init = () => {
     const renderer = (app.renderer = new THREE.WebGLRenderer({ antialias: true }));
@@ -195,13 +89,12 @@ const init = () => {
             const animations = gltf.animations;
             mixer = new THREE.AnimationMixer(model);
 
-            [idleAction, walkAction] = [mixer.clipAction(animations[0]), mixer.clipAction(animations[1])];
-            actions.idle.action = idleAction;
-            actions.walk.action = walkAction;
+            [deadAction, idleAction, walkAction] = [
+                mixer.clipAction(animations[0]),
+                mixer.clipAction(animations[1]),
+                mixer.clipAction(animations[2])
+            ];
             idleAction.play();
-            walkAction.play();
-            setWeight(idleAction, actions.idle.weight);
-            setWeight(walkAction, actions.walk.weight);
             createPanel();
             render();
         },
@@ -210,9 +103,6 @@ const init = () => {
             console.error(error);
         }
     );
-
-    stats = Stats();
-    app.el.appendChild(stats.dom);
 };
 
 const render = () => {
@@ -220,9 +110,6 @@ const render = () => {
     mixer.update(delta);
     app.renderer.render(app.scene, app.camera);
     app.controls.update();
-    // baseActions.idle.weight = idleAction.getEffectiveWeight();
-    // baseActions.walk.weight = walkAction.getEffectiveWeight();
-    stats.update();
     requestAnimationFrame(render);
 };
 
